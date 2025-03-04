b24b6352cb781b33455fa028938b01efc7657dcf
Airam Plasencia <airaplas@gmail.com>
2025-03-04 12:05:59 +0000

    Homepage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,50 +9,51 @@ function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav className=" p-4">
-    <div className="flex items-center justify-between">
-      {/* Logo */}
-      <div className="flex items-center">
-        <img src="/img/logo.png" alt="Logo" className="h-96 max-w-sm mr-4" />
+    <nav className="p-4">
+      <div className="flex items-center justify-between">
+        
+        <div className="flex items-center mr-2">
+          <img src="/img/logo.png" alt="Logo" className="h-40 max-w-sm" />
+        </div>
+
+        {/* Links */}
+        <div className="flex space-x-6 mx-auto">
+        <Link to="/">
+          <button className="text-gray hover:text-gray-300">Home</button>
+        </Link>
+          <Link to="/" className="text-gray hover:text-gray-300">Courses</Link>
+          <Link to="/teachers" className="text-gray hover:text-gray-300">Teachers</Link>
+          <Link to="/about" className="text-gray hover:text-gray-300">About</Link>
+          <Link to="/gallery" className="text-gray hover:text-gray-300">Gallery</Link>
+
+          <div className="flex space-x-4 ml-auto">
+          {isLoggedIn ? (
+            <>
+              <button onClick={logOutUser} className="text-gray hover:bg-gray-700 px-4 py-2 rounded">
+                Logout
+              </button>
+
+              <Link to="/profile">
+                <button className="text-gray hover:bg-gray-700 px-4 py-2 rounded">Profile</button>
+              </Link>
+
+              <span className="text-gray">{user && user.name}</span>
+            </>
+          ) : (
+            <>
+              <Link to="/signup">
+                <button className="text-gray hover:text-gray-300 ">Sign Up</button>
+              </Link>
+              <Link to="/login">
+                <button className="text-gray hover:text-gray-300 ">Login</button>
+              </Link>
+          </>
+        )}
+      </div>
+        </div>   
       </div>
 
-      {/* Links */}
-      <div className="flex space-x-6">
-        <Link to="/" className="text-gray hover:text-gray-300">Cursos</Link>
-        <Link to="/professores" className="text-gray hover:text-gray-300">Profesores</Link>
-        <Link to="/about" className="text-gray hover:text-gray-300">About</Link>
-        <Link to="/galeria" className="text-gray hover:text-gray-300">Galería</Link>
-      </div>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-
-      {isLoggedIn && (
-        <>
-          <button onClick={logOutUser}>Logout</button>
-
-          <Link to="/profile">
-            <button>Profile</button>
-            {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
-          </Link>
-
-          <span>{user && user.name}</span>
-        </>
-      )}
-
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>Sign Up</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
-          </Link>
-        </>
-      )}
-       </div>
+     
     </nav>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,29 @@
-import "./HomePage.css";
+import React from 'react';
 
 function HomePage() {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className="flex items-center justify-between p-10">
+      
+      <div className="w-1/2">
+        <h2 className="text-blue-500 text-4xl font-semibold mb-4">
+          Figure Drawing Lessons
+        </h2>
+        <p className="text-gray-700 text-xl">
+          THE BEST METHOD FOR BEGINNERS <br />
+          TO BECOME A PROFESSIONAL ARTIST
+        </p>
+      </div>
+
+      <div className="w-1/2">
+        <img
+          src="/img/home1.jpg"
+          alt="Figure Drawing"
+          className="max-w-full h-auto"
+        />
+      </div>
     </div>
   );
 }
 
 export default HomePage;
+
